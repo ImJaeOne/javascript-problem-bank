7ee63766fd999493df93f33644e292629376be2f
ImJaeOne <dlawi0108@naver.com>
2025-01-23 09:37:26 +0900

    part-02.배열 - 데이터-변환-파이프라인

--- part2/02.배열/(lv.3)데이터-변환-파이프라인.js
+++ part2/02.배열/(lv.3)데이터-변환-파이프라인.js
@@ -14,7 +14,9 @@
  * @returns {number[]} 변환된 배열
  */
 
-function transformData(arr, threshold) {}
+function transformData(arr, threshold) {
+    return [...arr].filter((a) => a % 2 === 0).map((a) => a * a).filter((a) => a <= threshold).sort((a, b) => b - a)
+}
 
 // export 를 수정하지 마세요.
 export { transformData };
